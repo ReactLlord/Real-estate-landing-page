4cce981eb0fdff2836d76b637d6196a17640302e
EBUNOLUWA <141589270+ReactLlord@users.noreply.github.com>
2025-04-16 20:49:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css';
 import Navbar from './components/Navbar'
 import Header from './components/Header';
-import About from './components/ABout';
+import About from './components/About';
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
